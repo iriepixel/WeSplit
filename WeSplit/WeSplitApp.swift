04d77e4b352e59edc5d70ef81c20df7c5dac70ff
Jev Smolnikov <jev@iriepixel.com>
2023-06-25 00:42:18 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jev Smolnikov on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
